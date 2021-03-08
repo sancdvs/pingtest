e2221aae473c847441226171aedbf4eddfa55672
sancdvs <2321310864@qq.com>
2021-03-08 20:16:07 +0800

    add

--- pingtest.py
+++ pingtest.py
@@ -1,8 +1,10 @@
 from pythonping import ping
 import time
+import datetime
 
 ip = ''
 file = ''
+log = ''
 
 def test():
     p = ping(ip)
@@ -14,6 +16,12 @@ def test():
             break
         else:
             print(0)
+    with open(log,'r+') as f:
+        content = f.read()
+        f.seek(0)
+        ps = str(p).split('\n')
+        s = str(datetime.datetime.now())+'\r'+ps[0]+ps[1]+ps[2]+ps[3]+ps[5]+'\r'
+        f.write(s+'\n'+content)
 
 
 
